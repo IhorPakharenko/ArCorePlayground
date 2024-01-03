afc8ca5484221cae2ec023b6d9cf6be3eaed8e15
Ihor Pakharenko <ihor.p@volpis.com>
2024-01-03 14:51:11 +0100

    AppArBackFacingFragment: properly load displayed model

--- app/src/main/java/com/example/arcoreplayground/ui/models/AppArBackFacingFragment.kt
+++ app/src/main/java/com/example/arcoreplayground/ui/models/AppArBackFacingFragment.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration
 import android.media.CamcorderProfile
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
@@ -18,14 +19,14 @@ import com.google.ar.core.Config
 import com.google.ar.core.HitResult
 import com.google.ar.core.Plane
 import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.SceneView
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.rendering.Renderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.BaseArFragment
 import com.google.ar.sceneform.ux.TransformableNode
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.future.await
 import kotlinx.coroutines.launch
 
@@ -34,7 +35,7 @@ class AppArBackFacingFragment : ArFragment() {
     private var model: Renderable? = null
     private val recorder: VideoRecorder = VideoRecorder()
 
-    private var modelPath = MutableStateFlow<String?>(null)
+    private var setModelJob: Job? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         arguments = (arguments ?: bundleOf()).apply {
@@ -53,27 +54,9 @@ class AppArBackFacingFragment : ArFragment() {
             config.instantPlacementMode = Config.InstantPlacementMode.LOCAL_Y_UP
         }
         setOnViewCreatedListener { arSceneView ->
-//                arSceneView.setFrameRateFactor(SceneView.FrameRate.FULL)
+            arSceneView.setFrameRateFactor(SceneView.FrameRate.FULL)
         }
         setOnTapArPlaneListener(::onTapPlane)
-
-        lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.CREATED) {
-                launch {
-                    modelPath.mapLatest { path ->
-                        ModelRenderable.builder()
-                            .setSource(context, Uri.parse(path))
-                            .setIsFilamentGltf(true)
-                            .build()
-                            .await()
-                    }.catch {
-
-                    }.collect {
-                        model = it
-                    }
-                }
-            }
-        }
     }
 
     private fun onTapPlane(hitResult: HitResult, plane: Plane, motionEvent: MotionEvent) {
@@ -94,7 +77,19 @@ class AppArBackFacingFragment : ArFragment() {
     }
 
     fun setModel(modelPath: String) {
-        this.modelPath.value = modelPath
+        val exceptionHandler = CoroutineExceptionHandler { _, exception ->
+            Log.e(AppArBackFacingFragment::class.simpleName, "Setting model failed", exception)
+        }
+        setModelJob?.cancel()
+        setModelJob = lifecycleScope.launch(exceptionHandler) {
+            repeatOnLifecycle(Lifecycle.State.CREATED) {
+                model = ModelRenderable.builder()
+                    .setSource(context, Uri.parse(modelPath))
+                    .setIsFilamentGltf(true)
+                    .build()
+                    .await()
+            }
+        }
     }
 
     suspend fun takeScreenshot(): Result<Uri> =
